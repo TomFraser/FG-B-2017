bbb66d77ee786dc36890eafe3107a53637290b87
tfraser <18135@student.bbc.qld.edu.au>
2016-09-30 09:43:05 +1000

    Added Motor Library, removed I2CController as we wont be needing it & added t3spi library (thanks btmcmahan on github)

---
+++ Software/lib/Motor/Motor.cpp
@@ -0,0 +1,21 @@
+#include <Motor.h>
+
+Motor::Motor(int pwm, int dir, int brk, int rev){
+    pinMode(pwn, OUTPUT);
+    pinMode(dir, OUTPUT);
+    pinMode(brk, OUTPUT);
+    reversed = rev;
+    r_pwm = pwm;
+    r_dir = dir;
+    r_brk = brk;
+}
+void Motor::set(int pwm){
+    digitalWrite(r_dir, pwm > 0 ? !reversed : reversed);
+    analogWrite(r_pwm, pwm > 0 ? pwm : -pwm);
+    digitalWrite(r_brk, LOW);
+}
+void Motor::brake(){
+    digitalWrite(r_dr, LOW);
+    analogWrite(r_pwm, 0);
+    digitalWrite(r_brk, HIGH);
+}
---
+++ Software/lib/Motor/Motor.h
@@ -0,0 +1,18 @@
+#ifndef Motor_h
+#define Motor_h
+
+class Motor{
+
+public:
+    Motor(int pwm, int dir, int brk, int rev);
+    void set(int pwm);
+    void brake();
+
+private:
+    int r_brk;
+    int r_pwm;
+    int r_dir;
+    int reversed;
+};
+
+#endif
