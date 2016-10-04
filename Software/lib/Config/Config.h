9368cccd9a1c7b9846df2e714cfd7b2015f1140b
tfraser <18135@student.bbc.qld.edu.au>
2016-10-04 13:47:21 +1000

    Nothing really

--- Software/lib/Config/Config.h
+++ Software/lib/Config/Config.h
@@ -8,4 +8,27 @@
 #define POWER_PIN_2 0
 #define READ_THRESHOLD 40
 
+//--------------------------------------------
+//Light
+
+#define LIGHT_1 0
+#define LIGHT_2 0
+#define LIGHT_3 0
+#define LIGHT_4 0
+#define LIGHT_5 0
+#define LIGHT_6 0
+#define LIGHT_7 0
+#define LIGHT_8 0
+#define LIGHT_9 0
+#define LIGHT_10 0
+#define LIGHT_11 0
+#define LIGHT_12 0
+#define LIGHT_13 0
+#define LIGHT_14 0
+#define LIGHT_15 0
+#define LIGHT_16 0
+#define LIGHT_17 0
+#define LIGHT_18 0
+#define LIGHT_19 0
+
 #endif
--- Software/lib/LightController/LightController.cpp
+++ Software/lib/LightController/LightController.cpp
@@ -2,8 +2,28 @@
 
 LightController::LightController(){
     //init
+
+    pinMode(LIGHT_1, INPUT);
+    pinMode(LIGHT_2, INPUT);
+    pinMode(LIGHT_3, INPUT);
+    pinMode(LIGHT_4, INPUT);
+    pinMode(LIGHT_5, INPUT);
+    pinMode(LIGHT_6, INPUT);
+    pinMode(LIGHT_7, INPUT);
+    pinMode(LIGHT_8, INPUT);
+    pinMode(LIGHT_9, INPUT);
+    pinMode(LIGHT_10, INPUT);
+    pinMode(LIGHT_11, INPUT);
+    pinMode(LIGHT_12, INPUT);
+    pinMode(LIGHT_13, INPUT);
+    pinMode(LIGHT_14, INPUT);
+    pinMode(LIGHT_15, INPUT);
+    pinMode(LIGHT_16, INPUT);
+    pinMode(LIGHT_17, INPUT);
+    pinMode(LIGHT_18, INPUT);
+    
 }
 
 void LightController::letsDoSomeGeometry(){
-    
+
 }
