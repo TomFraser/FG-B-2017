147e4c451c44360b195f9f7127bea164f44eceb6
tfraser <18135@student.bbc.qld.edu.au>
2016-10-10 19:48:46 +1000

    Small things not work explaining

--- Software/Tsop/src/TSOP.ino
+++ Software/Tsop/src/TSOP.ino
@@ -2,7 +2,7 @@
 #include <Config.h>
 #include <t3spi.h>
 
-TsopController tsops;
+ReadTSOPS tsops;
 
 volatile uint16_t dataIn[DATA_LENGTH];
 volatile uint16_t dataOut[DATA_LENGTH];
@@ -10,10 +10,10 @@ volatile uint16_t dataOut[DATA_LENGTH];
 T3SPI MOTOR;
 
 void setup(){
-    MOTOR.set_Slave(ALT_SCK, MOSI, MISO, CS0); //Might be wrong CS pin.
+    MOTOR.begin_SLAVE(ALT_SCK, MOSI, MISO, CS0); //Might be wrong CS pin.
     MOTOR.setCTAR_SLAVE(16, SPI_MODE0);
 }
 
 void loop(){
-    dataOut[0] = tsops.activateTsop();
+    dataOut[0] = tsops.moveTangent();
 }
