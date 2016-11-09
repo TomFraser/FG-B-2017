292eae1289bac75b321c7a0ad2fc684c83e7ced2
tfraser <18135@student.bbc.qld.edu.au>
2016-11-09 11:51:00 +1000

    Added Buzzer Library... Lol

---
+++ Software/lib/Buzzer/Buzzer.h
@@ -0,0 +1,35 @@
+#ifndef Buzzer_h
+#define Buzzer_h
+
+#include <Arduino.h>
+#include <Config.h>
+#include <Pins.h>
+#include <Defines.h>
+
+class Buzzer{
+
+public:
+    void playTone(int miliS);
+    void errorTone();
+    void readyTone();
+private:
+
+};
+
+void Buzzer::playTone(int miliS){
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(miliS);
+    digitalWrite(BUZZER_PIN, LOW);
+}
+
+void Buzzer::errorTone(){
+    digitalWrite(BUZZER_PIN, HIGH);
+}
+
+void Buzzer::readyTone(){
+    playTone(500);
+    delay(500);
+    playTone(500);
+}
+
+#endif
