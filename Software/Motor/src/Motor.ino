c8b1b7b14bfcb9b0a28a8c0cd2f21fd1cf7610fa
tfraser <18135@student.bbc.qld.edu.au>
2017-02-21 20:54:50 +1000

    Small functions for testing motors on thursday and removal of an accidental file

--- Software/Motor/src/Motor.ino
+++ Software/Motor/src/Motor.ino
@@ -13,6 +13,8 @@ DirectionController direction = DirectionController();
 
 double tsopAng = 0.00;
 
+int counter = 0;
+
 void setup(){
     // MASTER_TEENSY.begin_MASTER(ALT_SCK, MOSI, MISO, CS1, CS_ActiveLOW);
     // MASTER_TEENSY.setCTAR(CTAR_0, 16, SPI_MODE0, LSB_FIRST, SPI_CLOCK_DIV8);
@@ -27,4 +29,20 @@ void loop(){ //Things to do before running the program, setting up the pins!!!!!
     // kicker.kickerReady(); //Kicker
     // kicker.checkLightGate();
     // kicker.kickBall();
+}
+
+void testDirection(){
+    direction.setPWM(255);
+    delay(2000);
+    direction.setPWM(-255);
+    delay(2000);
+}
+
+void incrementSpeed(){
+    direction.setPWM(counter);
+    delay(100);
+    counter++;
+    if(counter >= 255){
+        counter = 0;
+    }
 }
