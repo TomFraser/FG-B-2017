78cbb8d4d095392ed8e2ae38ab070c1c462c7be2
Alistair English <alistair.english@bigpond.com>
2017-02-27 17:57:26 +1000

    light stuff

--- Software/lib/Light/Light.h
+++ Software/lib/Light/Light.h
@@ -20,6 +20,9 @@ public:
     //Set up the light sensors
     void init();
 
+    //Dump Light Values
+    void getVals(int *);
+
     //Read the light sensors
     void readLight();
 
@@ -42,9 +45,30 @@ private:
 
     //for reading light
     int detectedNumber;
-    bool seeingWhite[19];
+    bool seeingWhite[19] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false};
 
     //cluster stuff
+    double lightCoords[19][2] = {
+        {light1x, light1y},
+        {light2x, light2y},
+        {light3x, light3y},
+        {light4x, light4y},
+        {light5x, light5y},
+        {light6x, light6y},
+        {light7x, light7y},
+        {light8x, light8y},
+        {light9x, light9y},
+        {light10x, light10y},
+        {light11x, light11y},
+        {light12x, light12y},
+        {light13x, light13y},
+        {light14x, light14y},
+        {light15x, light15y},
+        {light16x, light16y},
+        {light17x, light17y},
+        {light18x, light18y},
+        {light19x, light19y}
+    };
 
 };
 #endif
