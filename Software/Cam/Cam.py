52b5a8d0e12b5c430f7a2a80ae8811adcf545a97
TomFraser <tr.fraser@bigpond.com>
2017-09-08 20:29:51 +1000

    Started work on code for the OpenCV M7 Camera

---
+++ Software/Cam/Cam.py
@@ -0,0 +1,31 @@
+# Untitled - By: Thomas - Fri Sep 8 2017
+from pyb import SPI
+from machine import Pin
+import sensor
+
+interruptPin = Pin(0, Pin.IN)
+
+interruptPin.irq(trigger=Pin.IQR_FALLING, handler=hasSPIRequest)
+
+spi = SPI(1, SPI.SLAVE, baudrate=600000, polarity=1, phase=0, crc=None)
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.skip_frames()
+
+getBallDirection(input):
+    if input == None:
+        return ballLoc
+    else:
+        ballLoc = input
+        
+def hasSPIRequest():
+    ballDirection = getBallDirection(input=None)
+    spi.send(ballDirection, timeout=100)
+
+while(True):
+    img = sensor.snapshot()
+    for block in img.find_blobs([ball_thresholds]):
+        ballCentre = [b.cx(), b.cy()]
+        getBallDirection(input=ballCentre)
