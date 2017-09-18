b7f7e7181f13713bede96c03296ca41a20ef9c84
TomFraser <tr.fraser@bigpond.com>
2017-09-18 12:20:24 +1000

    Added coopers passive ball tracking code, all that is left is to calulate orbits and perform SPI

--- Software/Cam/Cam.py
+++ Software/Cam/Cam.py
@@ -1,32 +1,33 @@
-# Cam - By: Thomas - Fri Sep 8 2017
-from pyb import SPI
-from machine import Pin
-import sensor
-
-interruptPin = Pin(0, Pin.IN)
-
-interruptPin.irq(trigger=Pin.IQR_FALLING, handler=hasSPIRequest)
-
-spi = SPI(1, SPI.SLAVE, baudrate=600000, polarity=1, phase=0, crc=None)
-
+import sensor, image, time, math
+threshold_index = 0
+thresholds = [(36, 69, 39, 89, 9, 43),
+              (30, 100, -64, -8, -32, 32),
+              (0, 30, 0, 64, -128, 0)]
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
-sensor.skip_frames()
-
-def getBallDirection(input=None):
-    if input == None:
-        return ballLoc
-    else:
-        #Do some cheeky calulation stuff
-        ballLoc = input
-
-def hasSPIRequest():
-    ballDirection = getBallDirection()
-    spi.send(ballDirection, timeout=100)
-
+sensor.skip_frames(time = 2000)
+sensor.set_auto_gain(False)
+sensor.set_auto_whitebal(False)
+clock = time.clock()
 while(True):
+    clock.tick()
+    x = 0
+    y = 0
+    strength = 0
+    angle = 0
     img = sensor.snapshot()
-    for block in img.find_blobs([ball_thresholds]):
-        ballCentre = [b.cx(), b.cy()]
-        getBallDirection(input=ballCentre)
+    for blob in img.find_blobs([thresholds[threshold_index]], pixels_threshold=20, area_threshold=20, merge=True):
+        img.draw_cross(blob.cx(), blob.cy())
+        x = -(blob.cx() - 160)
+        y = blob.cy() - 120
+        strength = math.sqrt(x*x + y*y)
+    angle = math.atan2(y,x) * (180 / math.pi) - 90
+    angle = angle%360
+    print("angle:")
+    print(angle)
+    print()
+    print("strength")
+    print(strength)
+    print()
+    print(clock.fps())
