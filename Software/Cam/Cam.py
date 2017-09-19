05a82e2f4f3cc85b6bb99457d67577707b4d18e7
Cooper Richmond <crich243@gmail.com>
2017-09-19 18:15:13 +1000

    more clean up / commented it better

--- Software/Cam/Cam.py
+++ Software/Cam/Cam.py
@@ -1,8 +1,9 @@
 import sensor, image, time, math, pyb
-from pyb import SPI, Pin, LED
+from pyb import SPI, Pin, LED, delay
+from math import atan2, sqrt, pi
 
 #Ball Thresholds
-thresholds = [(20, 30, 14, 31, 21, 45)]
+thresholds = [(55, 83, 0, 22, 44, 127)]
 
 #LED's
 ledRed = LED(1)
@@ -24,47 +25,50 @@ def spiSend():
     ledRed.on()
     recv = spi.recv()
     spi.send(angle,timeout=200)
+    delay(100)
     ledRed.off()
 
 #Attach Interupt
 Interupt = pyb.ExtInt(Pin('P3'), pyb.ExtInt.IRQ_FALLING, pyb.Pin.PULL_NONE, spiSend)
 
-spi.init(SPI.SLAVE)
-Interupt.enable()
-
+#Image Sensor Stuff
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
-sensor.set_framesize(sensor.QQVGA)
-sensor.skip_frames(time = 2000)
-sensor.set_auto_gain(False)
-sensor.set_auto_whitebal(False)
+sensor.set_framesize(sensor.QQVGA) #Resolution, QVGA = 42FPS,QQVGA = 85FPS
+sensor.skip_frames(time = 500) #Start Delay
+sensor.set_auto_gain(False) #Must remain false for blob tracking
+sensor.set_auto_whitebal(False) #Must remain false for blob tracking
 clock = time.clock()
 
+#LED's all turn off after boot up is done
 ledRed.off()
 ledGreen.off()
 ledBlue.off()
 
+#Main Loop
 while(True):
     clock.tick()
 
+    #Reset Variables
     x = 0
     y = 0
     strength = 0
 
+    #Find Ball
     img = sensor.snapshot()
-    for blob in img.find_blobs([thresholds[0]], pixels_threshold=20, area_threshold=20, merge=True):
+    for blob in img.find_blobs([thresholds[0]], pixels_threshold=10, area_threshold=10, merge=True):
         img.draw_cross(blob.cx(), blob.cy())
         x = -(blob.cx() - 80)
         y = blob.cy() - 60
-        strength = math.sqrt(x*x + y*y)
+        strength = sqrt(x*x + y*y) #Calculate Ball Distance
 
-    calcAngle = (math.atan2(y,x) * (180 / math.pi) - 90)%360
-
+    #If not seeing ball, angle = 65506, else calculate ball angle
     if strength == 0:
         angle = 65506
     else:
-        angle = calcAngle
+        angle = (atan2(y,x) * (180 / pi) - 90)%360
 
+    #Prints
     print("angle:")
     print(angle)
     print()
