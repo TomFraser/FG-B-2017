fecd28d2eb13fd06b462236fe8990fd1fef1b054
Alistair English <alistair.english@bigpond.com>
2017-10-09 16:52:54 +1000

    few changes to light clusters - didnt really change much

--- Software/Light/src/Light.cpp
+++ Software/Light/src/Light.cpp
@@ -14,10 +14,6 @@ volatile uint16_t dataOut[DATA_LENGTH] = {};
 double lightAngle;
 
 void transfer(){
-  if(Light.getError()){
-    digitalWrite(13, LOW);
-  }
-
   dataOut[0] = Light.getAngle();
   SPI0_PUSHR_SLAVE = dataOut[0]; //Push response to SPI Coms? Maybe?
   SPI0_SR |= SPI_SR_RFDF; //Signals end of transmission?
