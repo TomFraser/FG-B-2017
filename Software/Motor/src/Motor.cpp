ee4f4e7f59c08b23235582cd36d2af5200069288
Cooper Richmond <18212@bbc.qld.edu.au>
2017-10-25 11:53:10 +1000

    t3spi stuff

--- Software/Motor/src/Motor.cpp
+++ Software/Motor/src/Motor.cpp
@@ -1,13 +1,12 @@
 #include <Config.h>
 #include <MotorController.h>
-
+#include <t3spi.h>
 #include <Kicker.h>
 #include <Buzzer.h>
 #include <fgbcommon.h>
 #include <PixyI2C.h>
 #include <Motor.h>
 #include <Pins.h>
-#include <SPI.h>
 #include <LightTracker.h>
 #include <Blink.h>
 //
@@ -19,8 +18,8 @@
   #define KICK_THRESHOLD 980
 #endif
 
-volatile uint16_t dataOut[DATA_LENGTH] = {};
-volatile uint16_t dataIn[DATA_LENGTH] = {};
+volatile uint16_t dataOut[1] = {};
+volatile uint16_t dataIn[1] = {};
 
 long initialTime, currentTime, lastKick = 0;
 
@@ -28,6 +27,15 @@ long initialTime, currentTime, lastKick = 0;
 Kicker kicker = Kicker();
 LightTracker lightTracker = LightTracker();
 MotorController motorController = MotorController();
+T3SPI spi;
+
+uint16_t transaction(uint16_t command, int cs) {
+    dataOut[0] = command;
+    for (int i = 0; i < 3; i++) {
+        spi.txrx16(dataOut, dataIn, 1, CTAR_0, cs);
+    }
+    return dataIn[0];
+}
 
 void setup(){
     Wire1.begin(I2C_MASTER, 0x00, I2C_PINS_29_30, I2C_PULLUP_EXT, 19200);
@@ -39,11 +47,12 @@ void setup(){
     // //SPI SETUP
     pinMode(LIGHT_SS, OUTPUT);
     pinMode(TSOP_SS, OUTPUT);
-    SPI.begin();
-    digitalWrite(TSOP_SS, HIGH);
-    digitalWrite(LIGHT_SS, HIGH);
-    SPI.setSCK(ALT_SCK);
-    SPI.setClockDivider(SPI_CLOCK_DIV8);
+
+    spi.begin_MASTER(14, MOSI, MISO, TSOP_SS, CS_ActiveLOW);
+    spi.setCTAR(CTAR_0, 16, SPI_MODE0, LSB_FIRST, SPI_CLOCK_DIV16);
+
+    spi.enableCS(TSOP_SS, CS_ActiveLOW);
+    spi.enableCS(LIGHT_SS, CS_ActiveLOW);
     // defender.init();
 
 
@@ -54,31 +63,15 @@ void loop(){
     delay(MAIN_LOOP_DELAY);
 
     //SPI Transactions
-    digitalWrite(TSOP_SS, LOW);
-    delayMicroseconds(200);
-    int rotationData = SPI.transfer16(1);
-    rotationData = SPI.transfer16(1);
-    rotationData = SPI.transfer16(1);
-    digitalWrite(TSOP_SS, HIGH);
-
-    digitalWrite(LIGHT_SS, LOW);
-    delayMicroseconds(200);
-    int lightData = SPI.transfer16(255);
-    lightData = SPI.transfer16(255);
-    lightData = SPI.transfer16(255);
-    digitalWrite(LIGHT_SS, HIGH);
-
-    digitalWrite(TSOP_SS, LOW);
-    delayMicroseconds(200);
-    int tsopData = SPI.transfer16(0);
-    tsopData = SPI.transfer16(0);
-    tsopData = SPI.transfer16(0);
-    digitalWrite(TSOP_SS, HIGH);
-
-    // Serial.println(rotationData);
+    int tsopData = transaction(1, TSOP_SS);
+    int lightData = transaction(255, LIGHT_SS);
+    int rotationData = transaction(2, TSOP_SS);
+    int compassData = transaction(3, TSOP_SS);
+    int xData = transaction(4, TSOP_SS);
+    int yData = transaction(5, TSOP_SS);
 
     //Calculating absolute rotation
-    double rotation = rotationController.rotate(((rotationData-180)));
+    double rotation = rotationData - 180;
     double compass = (rotationData-180);
 
     //Calulating absolute angle
