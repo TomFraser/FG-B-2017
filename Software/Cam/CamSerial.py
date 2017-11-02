68444ab6ffcfdb4a1ea03441eaae7f527fb076aa
Alistair English <alistair.english@bigpond.com>
2017-11-02 17:19:48 +1000

    changed cam to find biggest blob, need to fix serial lol

--- Software/Cam/CamSerial.py
+++ Software/Cam/CamSerial.py
@@ -4,9 +4,9 @@ import ustruct, utime
 from math import atan2, sqrt, pi, log
 
 #Thresholds
-thresholds = [(55, 79, 21, 61, 2, 39), #Ball
-(0,0,0,0,0,0), #Goal 1
-(0,0,0,0,0,0)] # Goal 2
+thresholds = [(43, 83, 8, 69, 9, 50), #Ball
+(12,0,0,0,0,0), #Goal 1
+(12,0,0,0,0,0)] # Goal 2
 
 #LED's
 ledRed = LED(1)
@@ -58,47 +58,86 @@ while(True):
     x = 0
     y = 0
     strength = 0
-    Goal1size = 0
-    Goal2size = 0
+    goalAsize = 0
+    goalDsize = 0
 
     #Find Ball
     img = sensor.snapshot()
 
 
 
-    for ball in img.find_blobs([thresholds[0]], x_stride=2, y_stride=2, area_threshold=1, pixel_threshold=1, merge=False):
+    # find the ball
+    balls = img.find_blobs([thresholds[0]], x_stride=2, y_stride=2, area_threshold=1, pixel_threshold=1, merge=False)
+    if(len(balls) > 0):
+        ball = sorted(balls, key= lambda ball: ball.pixels(), reverse=True)[0]
+
+        # do the stuff
         img.draw_cross(ball.cx(), ball.cy())
         x = -(ball.cx() - (img.width() / 2)) #Calculate Coordinates of Ball
         y = ball.cy() - (img.height() / 2)
         angle = (atan2(y,x) * (180 / pi) - 90)%360
         strength = sqrt(x**2 + y**2)
 
-    for goal in img.find_blobs(thresholds, x_stride=10, y_stride=10, area_threshold=50, pixel_threshold=50, merge=True):
-        x = -(goal.cx() - (img.width() / 2)) #Calculate Coordinates of Ball
-        y = goal.cy() - (img.height() / 2)
-        s = sqrt(goal.pixels())
-        if goal.code() == 2 and s > 10: #2^1
-            img.draw_rectangle(goal.rect())
-            img.draw_cross(goal.cx(), goal.cy())
-            Goal1size =  sqrt(goal.pixels())
-            Goal1angle = (atan2(y,x) * (180 / pi) - 90)%360
+        # for ball in img.find_blobs([thresholds[0]], x_stride=2, y_stride=2, area_threshold=1, pixel_threshold=1, merge=False):
+        #     img.draw_cross(ball.cx(), ball.cy())
+        #     x = -(ball.cx() - (img.width() / 2)) #Calculate Coordinates of Ball
+        #     y = ball.cy() - (img.height() / 2)
+        #     angle = (atan2(y,x) * (180 / pi) - 90)%360
+        #     strength = sqrt(x**2 + y**2)
 
-        if goal.code() == 4 and s > 10: #2^2
-            img.draw_cross(goal.cx(), goal.cy())
-            Goal2size =  sqrt(goal.pixels())
-            Goal2angle = (atan2(y,x) * (180 / pi) - 90)%360
+    # find the goals
+    # goal A
+    goalsA = img.find_blobs([thresholds[1]], x_stride=10, y_stride=10, area_threshold=50, pixel_threshold=50, merge=True)
+    if(len(goalsA) > 0):
+        goalA = sorted(goalsA, key= lambda goal: goal.pixels(), reverse=True)[0]
+
+        if(sqrt(goalA.pixels())>5000):
+            x = -(goalA.cx() - (img.width() / 2))
+            y = goalA.cy() - (img.height() / 2)
+            goalAangle = (atan2(y,x) * (180 / pi) - 90)%360
+            goalAsize = sqrt(goalA.pixels())
+            img.draw_rectangle(goalA.rect())
+            img.draw_cross(goalA.cx(), goalA.cy())
+
+    # goal D
+    goalsD = img.find_blobs([thresholds[2]], x_stride=10, y_stride=10, area_threshold=50, pixel_threshold=50, merge=True)
+    if(len(goalsD)):
+        goalD = sorted(goalsD, key= lambda goal: goal.pixels(), reverse=True)[0]
+
+        if(sqrt(goalD.pixels())>5000):
+            x = -(goalD.cx() - (img.width() / 2))
+            y = goalD.cy() - (img.height() / 2)
+            goalDangle = (atan2(y,x) * (180 / pi) - 90)%360
+            goalDsize = sqrt(goalD.pixels())
+            img.draw_rectangle(goalD.rect())
+            img.draw_cross(goalD.cx(), goalD.cy())
+
+    # for goal in img.find_blobs(thresholds, x_stride=10, y_stride=10, area_threshold=50, pixel_threshold=50, merge=True):
+    #     x = -(goal.cx() - (img.width() / 2)) #Calculate Coordinates of Ball
+    #     y = goal.cy() - (img.height() / 2)
+    #     s = sqrt(goal.pixels())
+    #     if goal.code() == 2 and s > 10: #2^1
+    #         img.draw_rectangle(goal.rect())
+    #         img.draw_cross(goal.cx(), goal.cy())
+    #         goalAsize =  sqrt(goal.pixels())
+    #         goalAangle = (atan2(y,x) * (180 / pi) - 90)%360
+    #
+    #     if goal.code() == 4 and s > 10: #2^2
+    #         img.draw_cross(goal.cx(), goal.cy())
+    #         goalDsize =  sqrt(goal.pixels())
+    #         goalDangle = (atan2(y,x) * (180 / pi) - 90)%360
 
     #If not seeing ball, angle = 65506, else calculate ball angle
     if strength == 0: angle = 500
     if angle == 0: angle = 360
-    if Goal1size == 0: Goal1angle = 500
-    if Goal2size == 0: Goal2angle = 500
+    if goalAsize == 0: goalAangle = 500
+    if goalDsize == 0: goalDangle = 500
 
     ballData = [angle, strength]
-    goal1Data = [Goal1angle,int(Goal1size)]
-    goal2Data = [Goal2angle,int(Goal2size)]
+    goalAdata = [goalAangle,int(goalAsize)]
+    goalDdata = [goalDangle,int(goalDsize)]
 
-    #print(Goal1size)
+    #print(goalAsize)
 
 
 
@@ -115,23 +154,23 @@ while(True):
         sendBuff[1] = 255
         sendBuff[2] = int(angle % 255)
 
-    if goal1Data[0] <= 255:
-        sendBuff[3] = int(goal1Data[0])
+    if goalAdata[0] <= 255:
+        sendBuff[3] = int(goalAdata[0])
         sendBuff[4] = 0
     else:
         sendBuff[3] = 255
-        sendBuff[4] = int(goal1Data[0] % 255)
+        sendBuff[4] = int(goalAdata[0] % 255)
 
-    sendBuff[5] = goal1Data[1]
+    sendBuff[5] = goalAdata[1]
 
-    if goal2Data[0] <= 255:
-        sendBuff[6] = int(goal2Data[0])
+    if goalDdata[0] <= 255:
+        sendBuff[6] = int(goalDdata[0])
         sendBuff[7] = 0
     else:
         sendBuff[6] = 255
-        sendBuff[7] = int(goal2Data[0] % 255)
+        sendBuff[7] = int(goalDdata[0] % 255)
 
-    sendBuff[8] = goal2Data[1]
+    sendBuff[8] = goalDdata[1]
 
     for i in range(0,10):
         if sendBuff[i] == 42 and i != 0:
@@ -158,7 +197,7 @@ while(True):
     # uart.writechar(sendBuff[8])
     # uart.writechar(sendBuff[9])
 
-    print(sendBuff)
+    #print(sendBuff)
 
     pyb.delay(1)
 
